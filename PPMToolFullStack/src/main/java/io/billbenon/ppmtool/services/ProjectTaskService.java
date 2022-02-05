b645ab8fd0ca4f769101447d44b4f60e04c328d6
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-02-05 23:31:34 +0200

    Persist Project Task (Bug fix pending setPriority

---
+++ PPMToolFullStack/src/main/java/io/billbenon/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,34 @@
+package io.billbenon.ppmtool.services;
+
+import io.billbenon.ppmtool.domain.Backlog;
+import io.billbenon.ppmtool.domain.ProjectTask;
+import io.billbenon.ppmtool.repositories.BacklogRepository;
+import io.billbenon.ppmtool.repositories.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+    @Autowired
+    private BacklogRepository backlogRepository;
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        projectTask.setBacklog(backlog);
+        Integer BacklogSequence = backlog.getPTSequence();
+        BacklogSequence++;
+        projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+//        if (projectTask.getPriority() == 0 || projectTask.getPriority() == null) {
+//            projectTask.setPriority(3);
+//        }
+        if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+            projectTask.setStatus("TO_DO");
+        }
+        return projectTaskRepository.save(projectTask);
+
+    }
+}
