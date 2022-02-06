11795c0071460afd0dbde31aa43867e4cd575941
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-02-06 17:36:49 +0200

    Get Project Backlog (happy path)

--- PPMToolFullStack/src/main/java/io/billbenon/ppmtool/services/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/io/billbenon/ppmtool/services/ProjectTaskService.java
@@ -7,6 +7,8 @@ import io.billbenon.ppmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProjectTaskService {
     @Autowired
@@ -33,5 +35,9 @@ public class ProjectTaskService {
         }
         return projectTaskRepository.save(projectTask);
 
+    }
+
+    public Iterable<ProjectTask> findBacklogById(String id) {
+        return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
 }
