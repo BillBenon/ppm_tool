24960c5737392a6b62d761ed6725cd1375051c5a
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-02-11 10:46:13 +0200

    init: user registration

--- PPMToolFullStack/src/main/java/io/billbenon/ppmtool/services/UserService.java
+++ PPMToolFullStack/src/main/java/io/billbenon/ppmtool/services/UserService.java
@@ -1,7 +1,9 @@
 package io.billbenon.ppmtool.services;
 
+import io.billbenon.ppmtool.domain.User;
 import io.billbenon.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +11,15 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User saveUser(User newUser) {
+        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+
+        // username has to be unique (exception)
+
+        // Make sure that password and confirm password match
+        return userRepository.save(newUser);
+    }
 }
